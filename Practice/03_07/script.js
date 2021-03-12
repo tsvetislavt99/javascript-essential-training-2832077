5ce1cccb042aef2f5fd453ae0412dd556bcfe03b
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2021-03-12 20:51:24 +0200

    Update script.js
    First task done,
    created an object

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,18 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const macbook = {
+  model: "Macbook Air 13'3 M1 2337",
+  cpu: "M1",
+  ssd: "256GB",
+  ram: "8GB",
+  adapterConnected: false,
+  connectAdapter: function (adapterStatus) {
+    this, (this.adapterConnected = adapterStatus);
+  },
+  adapterModel: {
+    usbPorts: 4,
+    hdmi: false,
+  },
+};
