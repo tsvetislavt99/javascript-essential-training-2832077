ad08fcf06e681475e79bf932874af01495af4369
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2021-03-14 03:35:17 +0200

    Did some experiments and 1 more practice assignment

--- 04_01e/script.js
+++ 04_01e/script.js
@@ -17,24 +17,37 @@ const everydayPack = new Backpack(
   "December 5, 2018 15:00:00 PST"
 );
 
-const content = `
-  <main>
-    <article>
-      <h1>${everydayPack.name}</h1>
-      <ul>
-        <li>Volume: ${everydayPack.volume}</li>
-        <li>Color: ${everydayPack.color}</li>
-        <li>Age: ${everydayPack.backpackAge()}</li>
-        <li>Number of pockets: ${everydayPack.pocketNum}</li>
-        <li>Left strap length: ${everydayPack.strapLength.left}</li>
-        <li>Right strap length: ${everydayPack.strapLength.right}</li>
-        <li>Lid status: ${everydayPack.lidOpen}</li>
-      </ul>
-    </article>
-  </main>
-`;
+const mainElement = createEl("main", "", {});
+const articleElement = createEl("article", "", { classList: "container-xl" });
+const h1Element = createEl("h1", `${everydayPack.name}`, {
+  classList: "container-sm",
+});
+const ul = createEl("div", "", { classList: "container" });
+const rowVolume = createEl("div", `Volume: ${everydayPack.volume}`, {
+  classList: "row justify-content-md-center",
+});
+// const content = `
+//   <main>
+//     <article>
+//       <h1>${everydayPack.name}</h1>
+//       <ul>
+//         <li>Volume: ${everydayPack.volume}</li>
+//         <li>Color: ${everydayPack.color}</li>
+//         <li>Age: ${everydayPack.backpackAge()}</li>
+//         <li>Number of pockets: ${everydayPack.pocketNum}</li>
+//         <li>Left strap length: ${everydayPack.strapLength.left}</li>
+//         <li>Right strap length: ${everydayPack.strapLength.right}</li>
+//         <li>Lid status: ${everydayPack.lidOpen}</li>
+//       </ul>
+//     </article>
+//   </main>
+// `;
 
-document.body.innerHTML = content;
+mainElement.appendChild(articleElement);
+articleElement.appendChild(h1Element);
+articleElement.appendChild(ul);
+ul.appendChild(rowVolume);
+document.body.appendChild(mainElement);
 
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
