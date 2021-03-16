421aa6cd0d19557a6443b378426e270e7a07683f
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2021-03-17 00:30:47 +0200

    Functions practice and createEl tests
    The idea behind createEl is not to use .innerHTML at all

---
+++ Practice/08_06/data.js
@@ -0,0 +1,15 @@
+const greenBackpack = {
+  name: "Viper",
+  volume: 90,
+  color: "green",
+  pockets: 20,
+};
+
+const redBackpack = {
+  name: "Lava",
+  volume: 30,
+  color: "red",
+  pockets: 34,
+};
+
+export const backpacks = [greenBackpack, redBackpack];
--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,44 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+import { backpacks } from "./data.js";
+
+const mainEl = document.querySelector(".maincontent");
+
+console.log(mainEl);
+
+backpacks.forEach((backpack) => {
+  const articleEl = createEl("article", [
+    createEl("h1", backpack.name, { classList: "backpack-heading" }),
+    createEl("ul", [
+      createEl("li", backpack.color),
+      createEl("li", String(backpack.volume)),
+      createEl("li", String(backpack.pockets)),
+    ]),
+  ]);
+  mainEl.append(articleEl);
+});
+
+function createEl(type, content, attributes) {
+  const result = document.createElement(type);
+
+  if (attributes !== undefined) {
+    Object.assign(result, attributes);
+  }
+
+  if (Array.isArray(content)) {
+    content.forEach(append);
+  } else {
+    append(content);
+  }
+
+  function append(node) {
+    if (typeof node === "string") {
+      node = document.createTextNode(node);
+    }
+    result.appendChild(node);
+  }
+
+  return result;
+}
