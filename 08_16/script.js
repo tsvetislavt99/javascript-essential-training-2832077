10ba16acc890ae003a2cbb18246aa45761e4b27c
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2021-03-19 00:18:49 +0200

    finished functions sections and experimented a little with event listeners

--- 08_16/script.js
+++ 08_16/script.js
@@ -5,55 +5,48 @@
  * - Set the inner HTML of the article to the existing HTML output provided in const content.
  * - Append each backpack object to the <main> element.
  */
-import Backpack from "./components/Backpack.js";
+import backpackObjectArray from "./components/data.js";
 
-const everydayPack = new Backpack(
-  "pack01",
-  "Everyday Backpack",
-  30,
-  "grey",
-  15,
-  26,
-  26,
-  false,
-  "December 5, 2018 15:00:00 PST",
-  "../assets/images/everyday.svg"
-);
+const backpacksElements = backpackObjectArray.map((backpack) => {
+  const backpackEl = `
+  <figure class="backpack__image">
+    <img src=${backpack.image} alt="" />
+  </figure>
+  <h1 class="backpack__name">${backpack.name}</h1>
+  <ul class="backpack__features">
+    <li class="packprop backpack__volume">Volume:<span> ${
+      backpack.volume
+    }l</span></li>
+    <li class="packprop backpack__color">Color:<span> ${
+      backpack.color
+    }</span></li>
+    <li class="backpack__age">Age:<span> ${backpack.backpackAge()} days old</span></li>
+    <li class="packprop backpack__pockets">Number of pockets:<span> ${
+      backpack.pocketNum
+    }</span></li>
+    <li class="packprop backpack__strap">Left strap length:<span> ${
+      backpack.strapLength.left
+    } inches</span></li>
+    <li class="packprop backpack__strap">Right strap length:<span> ${
+      backpack.strapLength.right
+    } inches</span></li>
+    <li class="feature backpack__lid">Lid status:<span> ${
+      backpack.lidOpen ? "open" : "closed"
+    }</span></li>
+  </ul>
 
-const content = `
-    <figure class="backpack__image">
-      <img src=${everydayPack.image} alt="" />
-    </figure>
-    <h1 class="backpack__name">${everydayPack.name}</h1>
-    <ul class="backpack__features">
-      <li class="packprop backpack__volume">Volume:<span> ${
-        everydayPack.volume
-      }l</span></li>
-      <li class="packprop backpack__color">Color:<span> ${
-        everydayPack.color
-      }</span></li>
-      <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
-      <li class="packprop backpack__pockets">Number of pockets:<span> ${
-        everydayPack.pocketNum
-      }</span></li>
-      <li class="packprop backpack__strap">Left strap length:<span> ${
-        everydayPack.strapLength.left
-      } inches</span></li>
-      <li class="packprop backpack__strap">Right strap length:<span> ${
-        everydayPack.strapLength.right
-      } inches</span></li>
-      <li class="feature backpack__lid">Lid status:<span> ${
-        everydayPack.lidOpen ? "open" : "closed"
-      }</span></li>
-    </ul>
-  
 `;
+
+  return backpackEl;
+});
 
 const main = document.querySelector(".maincontent");
 
-const newArticle = document.createElement("article");
-newArticle.classList.add("backpack");
-newArticle.setAttribute("id", "everyday");
-newArticle.innerHTML = content;
+backpacksElements.forEach((element) => {
+  const newArticle = document.createElement("article");
+  newArticle.classList.add("backpack");
+  newArticle.setAttribute("id", "everyday");
+  newArticle.innerHTML = element;
 
-main.append(newArticle);
+  main.append(newArticle);
+});
--- Practice/09_04/script.js
+++ Practice/09_04/script.js
@@ -5,3 +5,48 @@
  * - Add an event listener to each grid cell to change its background color when it is clicked.
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
+
+const gridBox = document.querySelector(".grid");
+const resetBtn = document.querySelector(".button");
+
+gridBox.addEventListener("mouseenter", () => {
+  gridBox.classList.add("grid-hovered");
+});
+
+gridBox.addEventListener("mouseleave", () => {
+  gridBox.classList.remove("grid-hovered");
+});
+
+const littleBoxes = document.querySelectorAll(".cell");
+
+littleBoxes.forEach((box) => {
+  box.addEventListener("mouseenter", () => {
+    box.classList.add("cell-hovered");
+  });
+  box.addEventListener("click", () => {
+    box.classList.add("cell-clicked");
+  });
+});
+
+resetBtn.addEventListener("click", () => {
+  littleBoxes.forEach((box) => box.classList.remove("cell-hovered"));
+});
+
+const body = document.body;
+body.addEventListener("keydown", (event) => {
+  // event.code holds the current key pressed:
+  console.log(event.code);
+
+  // Test for KeyD (the "d" key)
+  if (event.code === "KeyD") {
+    body.style.backgroundColor === ""
+      ? (body.style.backgroundColor = "hsl(201, 34%, 13%)")
+      : (body.style.backgroundColor = "");
+  }
+});
+
+body.addEventListener("keydown", (event) => {
+  if (event.code === "KeyD") {
+    alert("BackgroundChanged");
+  }
+});
